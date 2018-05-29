b14f9fd860826d61be9aba481638df694c063652
Jordi Adan <jadan@makenai.es>
2018-05-29 20:18:09 +0200

    Added cisco functionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 import * as ciscospark from 'ciscospark';
 
 
@@ -11,12 +11,25 @@ import * as ciscospark from 'ciscospark';
 export class HomePage {
 
   private spark;
+  private access_token = "NWM0Njk4ZmEtZjEyMC00YzIzLWE2NDAtMmQwMWQxM2VlOTFmZTJkYzlmNjEtYWFh";
 
   constructor(public navCtrl: NavController) {
 
   }
 
-  connect(){
+  connect() {
+    alert("Connecting...");
+    //this.test_promise()
+    this.connect_and_register()
+  }
+
+  test_promise(){
+    var test = new Promise(function(){});
+    test.constructor = Object;
+    Promise.resolve(test);
+  }
+
+  connect_and_register(){
     alert("Connecting...");
     if (!this.spark) {
       this.spark = ciscospark.init({
@@ -31,10 +44,65 @@ export class HomePage {
           }
         },
         credentials: {
-          access_token: "jkjkfdjfkdñajñda"/*this.access_token*/
+          access_token: this.access_token
         }
       });
     }
-  }
 
+    if (!this.spark.phone.registered) {
+      // we want to start listening for incoming calls *before* registering with
+      // the cloud so that we can join any calls that may already be in progress.
+      // this.spark.phone.on('call:incoming', (call) => {
+      //   Promise.resolve()
+      //     .then(() => {
+      //       // Let's render the name of the person calling us. Note that calls
+      //       // from external sources (some SIP URIs, PSTN numbers, etc) may not
+      //       // have personIds, so we can't assume that field will exist.
+      //       if (call.from && call.from.personId) {
+      //         // In production, you'll want to cache this so you don't have to do
+      //         // a fetch on every incoming call.
+      //         return this.spark.people.get(call.from.personId);
+      //       }
+      //
+      //       return Promise.resolve();
+      //     })
+      //     .then((person) => {
+      //       const str = person ? `Anwser incoming call from ${person.displayName}` : 'Answer incoming call';
+      //       if (confirm(str)) {
+      //         call.answer();
+      //         //this.bindCallEvents(call);
+      //       }
+      //       else {
+      //         call.decline();
+      //       }
+      //     })
+      //     .catch((err) => {
+      //       console.error(err);
+      //       alert(err);
+      //     });
+      // });
+      console.log('trying to connect...');
+      return this.spark.phone.register()
+        .then(() => {
+          // This is just a little helper for our selenium tests and doesn't
+          // really matter for the example
+          document.body.classList.add('listening');
+          console.log('connected');
+          //document.getElementById('connection-status').innerHTML = 'connected';
+          console.log('calling jordi...');
+          this.spark.phone.dial('jadan@makenai.es');
+        })
+        // This is a terrible way to handle errors, but anything more specific is
+        // going to depend a lot on your app
+        .catch((err) => {
+          console.error(err);
+          alert(err.stack);
+          // we'll rethrow here since we didn't really *handle* the error, we just
+          // reported it
+          throw err;
+        });
+    }
+
+    return Promise.resolve();
+  }
 }
