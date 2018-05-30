09a0f75b9e59de185addd4204a264a38b13a5f41
Jordi Adan <jadan@makenai.es>
2018-05-30 13:02:47 +0200

    Removed declaration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-import { Camera } from '@ionic-native/camera';
 
 
 import { MyApp } from './app.component';
@@ -13,7 +12,6 @@ import { HomePage } from '../pages/home/home';
   declarations: [
     MyApp,
     HomePage,
-    Camera
   ],
   imports: [
     BrowserModule,
